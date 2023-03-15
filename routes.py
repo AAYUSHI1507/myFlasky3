1bf47c814701aa030d6e3f01ccb940e676d6d510
AAYUSHI1507 <a.kataria@somaiya.edu>
2023-03-16 04:31:13 +0530

    updated and created css file and mostly work on home page

--- routes.py
+++ routes.py
@@ -1,6 +1,18 @@
 from app import app
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/sarc_detector')
+def sarc_detector():
+    return render_template('sarc_detector.html')
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
